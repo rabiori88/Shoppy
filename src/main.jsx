dc5c8a5871640f450685b6e92084715989c4af4c
Rabiori <ctangtangbbang@gmail.com>
2023-11-06 22:54:41 +0900

    헤더 기본 세팅

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import NotFound from './pages/NotFound.jsx'
+import Home from './pages/Home.jsx'
+import AllProducts from './pages/AllProducts.jsx'
+import NewProduct from './pages/NewProduct.jsx'
+import ProductDetail from './pages/ProductDetail.jsx'
+import MyCart from './pages/MyCart.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {index: true, path: '/', element: <Home />},
+      {path: '/products', element: <AllProducts />},
+      {path: '/products/new', element: <NewProduct />},
+      {path: '/products/:id', element: <ProductDetail />},
+      {path: '/carts', element: <MyCart />},
+    ]
+
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
---
+++ src/pages/MyCart.jsx
@@ -0,0 +1,6 @@
+
+export default function MyCart() {
+  return (
+    <div>MyCart</div>
+  )
+}
